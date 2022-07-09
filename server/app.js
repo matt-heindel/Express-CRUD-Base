f16f3646725bb02190e9ee3ebdfc8d4a413269bc
Matt Heindel <mheindel94@gmail.com>
2022-07-08 19:46:38 -0600

    Accept JSON from POST requests

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
-const app = require("express")();
+const express = require("express");
 const morgan = require("morgan");
+
+const app = express();
+
+// parse request body
+app.use(express.json());
 
 // configure logging, dev mode by default
 const env = process.env.ENV || "dev";
@@ -8,6 +13,11 @@ app.use(morgan(env));
 // route handlers
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 module.exports = app;
